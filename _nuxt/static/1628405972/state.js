8194b9586e92d5792e9daa3fca8b461b7e16b5ff
stephen-bunn <stephen-bunn@users.noreply.github.com>
2021-08-08 06:59:32 +0000

    deploy: dbf9b4e8d1d9e256dea8728fdfc84a6d83265b89

---
+++ _nuxt/static/1628405972/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbunn.io\u002F_nuxt\u002Fstatic\u002F1628405972",layout:"default",error:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fbunn.io\u002F",assetsPath:"\u002Fbunn.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"779b3588"}}}}(null));
