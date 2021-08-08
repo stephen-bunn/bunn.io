dbf9b4e8d1d9e256dea8728fdfc84a6d83265b89
Stephen Bunn <stephen@bunn.io>
2021-08-08 02:58:04 -0400

    Attempting to move around router config to fix reload issue
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/bunn.io/',
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Stephen Bunn',
