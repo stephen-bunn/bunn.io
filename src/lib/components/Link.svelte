fa4c50cd6ff9d34ec6dde5a2bed86bdbd0959e3a
Stephen Bunn <stephen@bunn.io>
2023-12-29 08:32:26 -0500

    Adding a base set of starting styles and fonts
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<a {href} {...$$restProps}><slot /></a>
+
+<style lang="scss">
+  a {
+    text-decoration: none;
+    transition: color var(--transition-duration-default);
+    outline: none;
+
+    &:link,
+    &:visited {
+      color: var(--color-link);
+    }
+
+    &:hover,
+    &:active,
+    &:focus-visible {
+      color: var(--color-link--active);
+    }
+
+    &:focus-visible {
+      outline: var(--color-current) dashed var(--space-halfx);
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+  import Link from "$lib/components/Link.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <Link href="https://kit.svelte.dev">kit.svelte.dev</Link> to read the documentation
+</p>
