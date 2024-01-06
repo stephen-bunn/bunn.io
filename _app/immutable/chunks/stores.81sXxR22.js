c0d91d2e9a0f688cd9f809e1f585d6cb1f26606b
stephen-bunn <stephen-bunn@users.noreply.github.com>
2024-01-06 20:26:14 +0000

    deploy: 546a5007d3188b2f0715140b94d903221b4e8cb9

---
+++ _app/immutable/chunks/stores.81sXxR22.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.4Nh_-6Mt.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
