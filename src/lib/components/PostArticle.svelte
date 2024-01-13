cf8c51d981e636f6e083cf3c703d84a876f91e45
Stephen Bunn <stephen@bunn.io>
2024-01-13 08:46:27 -0500

    Pulling out plot article content into a shared component
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

---
+++ src/lib/components/PostArticle.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Post } from "$lib/types/post"
+  import { formatDate } from "$lib/utils/format"
+  import Link from "$lib/components/Link.svelte"
+
+  export let post: Post
+</script>
+
+<article>
+  <span class="detail"
+    >Published in <Link small href={post.plot.href}>{post.plot.name}</Link>
+    on {formatDate(post.published, "MMM DD YYYY")}.</span
+  >
+  <h1>{post.title}</h1>
+  <slot />
+</article>
+
+<style lang="scss">
+  .detail {
+    font-size: var(--font-050);
+    color: var(--color-text-disabled);
+    font-family: var(--font-serif);
+  }
+</style>
--- src/routes/p/travel/[slug]/+page.svelte
+++ src/routes/p/travel/[slug]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import { formatDate } from "$lib/utils/format"
-  import Link from "$lib/components/Link.svelte"
+  import PostArticle from "$lib/components/PostArticle.svelte"
 
   export let data
   const { post, Content } = data
@@ -10,19 +9,6 @@
   <title>{post.title}</title>
 </svelte:head>
 
-<article>
-  <span class="detail"
-    >Published in <Link small href={post.plot.href}>{post.plot.name}</Link>
-    on {formatDate(post.published, "MMM DD YYYY")}.</span
-  >
-  <h1>{post.title}</h1>
+<PostArticle {post}>
   <Content />
-</article>
-
-<style lang="scss">
-  .detail {
-    font-size: var(--font-050);
-    color: var(--color-text-disabled);
-    font-family: var(--font-serif);
-  }
-</style>
+</PostArticle>
