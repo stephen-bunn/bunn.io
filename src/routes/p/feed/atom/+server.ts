a0350d56884056a36de2b2180a5e2aaca433a009
Stephen Bunn <stephen@bunn.io>
2024-02-04 10:35:00 -0500

    Re-enabling feed links
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- src/routes/p/feed/atom/+server.ts
+++ src/routes/p/feed/atom/+server.ts
@@ -4,5 +4,5 @@ import fetchPosts from "$lib/api/plots/travel"
 export const GET = async () => {
   // Limiting feed to the latest 10 posts
   const feed = buildFeed((await fetchPosts()).slice(0, 10))
-  return new Response(feed.atom1())
+  return new Response(feed.atom1(), { headers: { "Content-Type": "application/xml" } })
 }
