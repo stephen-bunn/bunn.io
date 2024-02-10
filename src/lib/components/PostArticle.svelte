b5ec6c64ecdb34e18e0ca9d880015ff21f59907c
Stephen Bunn <stephen@bunn.io>
2024-02-10 08:42:50 -0500

    Adding functionality to display the post's completeness as an emoji
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- src/lib/components/PostArticle.svelte
+++ src/lib/components/PostArticle.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
+  import { Tooltip } from "@svelte-plugins/tooltips"
   import type { Post } from "$lib/types/post"
+  import { PostStage, getPostStage } from "$lib/utils/post"
   import { formatDate } from "$lib/utils/format"
   import Link from "$lib/components/Link.svelte"
 
   export let post: Post
+  const postStage = getPostStage(post.metadata?.stage)
+
+  let stageEmoji = "🌱"
+  let stageDetail = "Growing; expect many changes"
+  if (postStage === PostStage.BUDDING) {
+    stageEmoji = "🌿"
+    stageDetail = "Taking form; not yet fully grown"
+  } else if (postStage === PostStage.EVERGREEN) {
+    stageEmoji = "🌳"
+    stageDetail = "Fully grown"
+  }
 </script>
 
 <article>
   <h1>{post.title}</h1>
   <span class="detail"
     >Published in <Link small href={post.plot.href}>{post.plot.name}</Link>
-    on {formatDate(post.published, "MMM DD YYYY")}.</span
-  >
+    on {formatDate(post.published, "MMM DD YYYY")}.
+    <span class="stage">
+      <Tooltip content={stageDetail} arrow={false} position="right" maxWidth={300}>
+        {stageEmoji}
+      </Tooltip>
+    </span>
+  </span>
   <slot />
 </article>
 
@@ -24,5 +42,10 @@
     font-size: var(--font-050);
     color: var(--color-text-disabled);
     font-family: var(--font-serif);
+  }
+
+  .stage {
+    margin-left: var(--space-1x);
+    cursor: default;
   }
 </style>
