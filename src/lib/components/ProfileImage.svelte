8ce0880926eb6799782357e1391357ce003b5060
Stephen Bunn <stephen@bunn.io>
2025-01-11 11:40:17 -0500

    feat: adding basic profile image component

---
+++ src/lib/components/ProfileImage.svelte
@@ -0,0 +1,31 @@
+<svelte:options runes />
+
+<script lang="ts">
+  import { FIRST_NAME } from '$lib/constants'
+
+  let { size = '256px', circle = false }: { size?: string; circle?: boolean } = $props()
+
+  const IMAGE_SRC = '/img/profile.jpeg'
+  const IMAGE_ALT = `Profile image of ${FIRST_NAME}`
+
+  let imageClass = $derived.by(() => {
+    return ['ProfileImage', circle && 'ProfileImage-circle'].filter(Boolean).join(' ')
+  })
+  let imageStyle = $derived(`width: ${size}; height: ${size};`)
+</script>
+
+<div class="ProfileImage_Container">
+  <img src={IMAGE_SRC} alt={IMAGE_ALT} class={imageClass} style={imageStyle} />
+</div>
+
+<style lang="scss">
+  .ProfileImage {
+    &_Container {
+      display: block;
+    }
+
+    &-circle {
+      border-radius: 50%;
+    }
+  }
+</style>
