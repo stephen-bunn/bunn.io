9a96bd796ff55a967fc17da1e58b93e5cfb9e33b
Stephen Bunn <stephen@bunn.io>
2025-01-15 17:23:05 -0500

    feat: handling meta tags through svelte-meta-tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <svelte:options runes />
 
 <script lang="ts">
+  import { MetaTags, deepMerge, type MetaTagsProps } from 'svelte-meta-tags'
+  import { page } from '$app/state'
   import '$lib/styles/index.scss'
 
-  let { children } = $props()
+  let { data, children } = $props()
+  let metaTags: MetaTagsProps = $derived(deepMerge(data.baseMetaTags, page.data.pageMetaTags))
 </script>
 
-<div>
-  {@render children()}
-</div>
+<MetaTags {...metaTags} />
+
+{@render children()}
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,29 @@
+import { FIRST_NAME, FULL_NAME, LAST_NAME, SITE_DESCRIPTION, SITE_NAME } from '$lib/constants'
+import type { MetaTagsProps } from 'svelte-meta-tags'
+
 export const prerender = true
 export const trailingSlash = 'always'
+
+export const load = ({ url }) => {
+  const baseMetaTags = Object.freeze({
+    title: FULL_NAME,
+    titleTemplate: `${FULL_NAME} &ndash; %s`,
+    description: SITE_DESCRIPTION,
+    canonical: new URL(url.pathname, url.origin).href,
+    keywords: [],
+    openGraph: {
+      type: 'website',
+      url: new URL(url.pathname, url.origin).href,
+      locale: 'en_US',
+      title: FULL_NAME,
+      site_name: SITE_NAME,
+      description: SITE_DESCRIPTION,
+      profile: {
+        firstName: FIRST_NAME,
+        lastName: LAST_NAME
+      }
+    },
+    additionalMetaTags: [{ property: 'proven.lol', content: 'proven3f2375' }]
+  }) satisfies MetaTagsProps
+  return { baseMetaTags }
+}
