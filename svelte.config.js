495a7e98e3793149a7351699dc0e257406748227
Stephen Bunn <stephen@bunn.io>
2025-01-25 07:37:13 -0500

    feat: adding sitemap.xml generation to static page build

--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,7 @@ const config = {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: '404.html',
+      fallback: '200.html',
       precompress: false,
       strict: true
     })
